6e13ad7d19cd1c31848ef0535f3dc2134ad5c813
iron <zj0713001@gmail.com>
2015-10-18 20:28:58 +0800

    *feature): change result to hash && add locate_in_china?(ip)

--- lib/ipip.rb
+++ lib/ipip.rb
@@ -6,13 +6,21 @@ module IPIP
   class << IPIP
 
     def find(ip)
-      ipv4_database.find(ip)
+      locations = ipv4_database.find(ip).to_s.split
+      {
+        country: locations[0],
+        province: locations[1],
+        city: locations[2],
+      }
     end
 
     def ipv4_database(data_file=nil)
       @ipv4_database ||= IPv4Database.new(data_file)
     end
 
+    def locate_in_china?(ip)
+      self.find(ip)[:country] == '中国'
+    end
   end
 
 end
